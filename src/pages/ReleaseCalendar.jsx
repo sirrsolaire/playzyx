51edbe50c2a28b9d761721ec66500ddf7198691c
Erkut Gürkan <111708975+sirrsolaire@users.noreply.github.com>
2023-10-04 00:22:37 +0300

    new component

--- src/pages/ReleaseCalendar.jsx
+++ src/pages/ReleaseCalendar.jsx
@@ -3,17 +3,27 @@ import { DesktopSideMenu } from "../ui/DesktopSideMenu.jsx";
 import { Title } from "../ui/Title.jsx";
 import SideMenu from "../ui/SideMenu.jsx";
 import { FloatSideMenu } from "../ui/FloatButton.jsx";
-import ReleaseCalendarContent from "../ui/ReleaseCalendarContent.jsx";
+import GamesByMonth from "../ui/GamesByMonth.jsx";
+import { currentYear } from "../helpers/dateConvertor.js";
+import { useSelector } from "react-redux";
+import { fullMonths } from "../helpers/monthStringConvertor.js";
+import { Calendar } from "../ui/Calendar.jsx";
 
 export const ReleaseCalendar = () => {
+  const getMonth = useSelector((state) => state.month.monthNo);
+
   return (
     <>
       <Header />
       <div className="tablet:flex tablet:px-10">
         <DesktopSideMenu />
         <section className="flex-col px-0 tablet:mt-6 tablet:flex tablet:w-full tablet:px-0">
-          <Title title="Release calendar" />
-          <ReleaseCalendarContent />
+          <Title
+            title={`Release calendar - ${fullMonths(getMonth)} ${currentYear}`}
+          />
+          <Calendar />
+
+          <GamesByMonth />
         </section>
         <SideMenu />
         <FloatSideMenu />
